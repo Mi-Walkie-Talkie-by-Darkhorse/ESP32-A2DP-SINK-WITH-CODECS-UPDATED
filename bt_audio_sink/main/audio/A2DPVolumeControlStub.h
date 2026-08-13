c4d524d23d49aa39e276e788fd1f1b8a81857c62
Darkhorse <Mi-Walkie-Talkie-by-Darkhorse@der-ball-ist-rund.net>
2026-08-13 13:56:33 +0300

    Use true exponent function instead of approximation for volume adjustment

--- bt_audio_sink/main/audio/A2DPVolumeControlStub.h
+++ bt_audio_sink/main/audio/A2DPVolumeControlStub.h
@@ -2,7 +2,6 @@
 
 #include <algorithm>
 #include "A2DPVolumeControl.h"
-#include "esp_log.h"
 
 class A2DPVolumeControlStub : public A2DPVolumeControl {
 public:
@@ -13,15 +12,18 @@ public:
     float get_normalized_volume_factor() { return normalizedVolumeFactor; }
 
     void set_volume(uint8_t volume) override {
-        constexpr float A = 0.3456789; // Here we adjust the curve startup sector from "0" point.
-        constexpr float B = 7.5;       // Here we adjust the curve middle sector.
-        constexpr float C = 6.75E-13;  // Here we adjust the curve final point "4096"
-        constexpr float kNorm = 1.0 / (127 * A + powf(127, B) * C);
+        constexpr unsigned maxVolume = 127u;
+        constexpr unsigned maxVolumeFactor = 4095u;
+
+        constexpr float kSlope = 4.0f;
+        constexpr float kInput = kSlope / maxVolume;
+        constexpr float kNorm = 1.0 / expm1f(kSlope);
+        constexpr float kVolumeFactor = (maxVolumeFactor + 1.0f) / expm1f(kSlope);
 
         float volumeFactorFloat = std::min((unsigned)volume, 127u);
-        volumeFactorFloat = volumeFactorFloat * A + powf(volumeFactorFloat, B) * C;
-        volumeFactor = std::min((unsigned)volumeFactorFloat, 4095u);
+        volumeFactorFloat = expm1f(volumeFactorFloat * kInput);
         normalizedVolumeFactor = volumeFactorFloat * kNorm;
+        volumeFactor = std::min((unsigned)(volumeFactorFloat * kVolumeFactor), maxVolumeFactor);
     }
 
 protected:
